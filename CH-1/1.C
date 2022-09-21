766eb480e9e93d84e91abf02573d21305ce2476a
Mann Meruliya <meruliyamann26100@gmail.com>
2022-09-21 17:26:07 +0530

    upload

---
+++ CH-1/1.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("NAME : Mann Meruliya\n");
+	printf("AGE : 17\n");
+	printf("SCHOOL : Shri Swaminarayan Mission");
+	getch();
+}
---
+++ CH-1/3.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("  - - - \n");
+	printf(" |     |\n" );
+	printf(" M     |\n" );
+	printf(" A     |\n" );
+	printf(" N     |\n" );
+	printf(" N     |\n" );
+	printf(" |     |\n" );
+	printf("  - - - " );
+	getch();
+}
